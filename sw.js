a6d07c107a802030dd8beeeb33358b42e3e23553
InfoLearn <infolearn8441@gmail.com>
2025-12-13 23:42:10 +0200

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("npl-devs-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "app.html",
+        "style.css",
+        "auth.css",
+        "icon-npl.png"
+      ]);
+    })
+  );
+});
+
 self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
